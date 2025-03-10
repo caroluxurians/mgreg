6619b8bc0d0977ed4f2299bf3c510f6bf12980d6
Žofie Tůmová <zofietumova@gmail.com>
2025-03-10 16:27:38 +0100

    header mobile init

---
+++ app/components/HeaderMobile.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const HeaderMobile = () => {
+  return (
+    <div className="flex justify-between items-center mx-5 mt-7 border-b-2 border-dark-pink">
+      <div className="font-heading text-dark-pink text-[25px] tracking-[0.25px]">
+        Markéta Gregorová
+      </div>
+      <Image src="/hamburger.png" height={10} width={25} alt="drop-down menu" />
+
+    </div>
+  );
+};
+
+export default HeaderMobile;
---
+++ app/moje-prace/page.tsx
@@ -0,0 +1,12 @@
+import HeaderMobile from "../components/HeaderMobile";
+
+const MojePrace = () => {
+  return (
+    <div className="h-screen max-h-screen bg-light-pink">
+      <HeaderMobile />
+      MOJE PRÁÁÁCE
+    </div>
+  );
+};
+
+export default MojePrace;

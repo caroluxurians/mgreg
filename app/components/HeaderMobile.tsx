7c8c978f01925c29ea14b85a65dbfb5af5bf932a
Žofie Tůmová <zofietumova@gmail.com>
2025-03-10 17:24:35 +0100

    moje-prace mobile init

--- app/components/HeaderMobile.tsx
+++ app/components/HeaderMobile.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const HeaderMobile = () => {
   return (
-    <div className="flex justify-between items-center mx-5 mt-7 border-b-2 border-dark-pink">
+    <div className="flex justify-between items-center mx-5 mt-7 border-b-1 border-dark-pink">
       <div className="font-heading text-dark-pink text-[25px] tracking-[0.25px]">
         Markéta Gregorová
       </div>
--- app/moje-prace/page.tsx
+++ app/moje-prace/page.tsx
@@ -1,10 +1,38 @@
+import Image from "next/image";
 import HeaderMobile from "../components/HeaderMobile";
 
 const MojePrace = () => {
   return (
-    <div className="h-screen max-h-screen bg-light-pink">
+    <div className="h-screen max-h-screen bg-light-pink text-dark-pink">
       <HeaderMobile />
-      MOJE PRÁÁÁCE
+      <main className="mx-5 pt-7 flex flex-col gap-5">
+        <div className="font-heading flex flex-col text-[30px]/[30px]">
+          <span>Ve svém mandátu se zaměřuji</span>
+          <span>především na naši bezpečnost</span>
+          <span>a naše digitální svobody.</span>
+        </div>
+        <div className="text-[14px]">
+          Od roku 2019 se věnuji regionu <br />
+          Východního partnerství, včetně Ukrajiny,<br />
+          hájím lidská práva ve světě a zaměřuji <br />
+          se na stále ještě opomíjený vliv nových<br />
+          technologií na naši bezpečnost a demokracii:<br />
+          ať již v podobě zbraní dvojího užití,<br />
+          dezinformací, cizího vměšování<br />
+          či umělé inteligence. Zároveň se snažím <br />
+          uhájit decentralizovaný, anonymní<br />
+          a svobodný Internet, kde není prolamováno<br />
+          soukromí běžných občanů.<br />
+        </div>
+        <Image
+          src="/marketa-icon-large.png"
+          height={309}
+          width={309}
+          alt="Markéta large icon"
+          className="mx-auto mt-5"
+          unoptimized
+        />
+      </main>
     </div>
   );
 };

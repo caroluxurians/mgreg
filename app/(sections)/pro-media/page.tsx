d88c279100bdc65de19b1c6585e19de6e3904ae0
Žofie Tůmová <zofietumova@gmail.com>
2025-03-23 14:53:44 +0100

    sm breakpoint init

--- app/(sections)/pro-media/page.tsx
+++ app/(sections)/pro-media/page.tsx
@@ -11,20 +11,20 @@ const ProMedia = () => {
       <HeaderMobile />
       <Header />
       <main className="flex flex-col">
-        <div className="w-[350px] mx-auto">
-          <div className="border-b border-red-pink mb-3">
-            <h2 className="font-heading text-[35px] mt-3 pb-1">
+        <div className="w-[350px] mx-auto sm:w-[560px] relative sm:mb-10">
+          <div className="border-b border-red-pink mb-3 sm:mb-4 relative">
+            <h2 className="font-heading text-[35px] mt-3 pb-1 sm:mt-6 sm:text-[40px]">
               Kontakty
             </h2>
           </div>
-          <div className="text-[14px]/[24px] mb-16">
+          <div className="text-[14px]/[24px] sm:text-[16px]/[26px] mb-16 sm:mb-8">
             Fusce aliquam vestibulum ipsum. Quis autem vel<br />
             eum iure repreh enderit qui in ea voluptate velit<br />
             esse quam nihil molestiae consequatur, vel illum<br />
             qui dolorem eum fugiat.<br />
           </div>
-          <div className="flex flex-col gap-5 mb-8 relative">
-            <div className="flex absolute z-3 right-10 -top-18">
+          <div className="flex flex-col gap-5 mb-8 sm:flex-row">
+            <div className="flex absolute z-3 right-10 top-42 sm:top-7">
               <Sticker
                 backgroundColor="bg-dark-pink"
                 textColor="text-beige"
@@ -52,13 +52,13 @@ const ProMedia = () => {
         </div>
 
         <div className="bg-beige py-10 mb-5">
-          <div className="w-[350px] mx-auto">
+          <div className="w-[350px] mx-auto sm:w-[560px] sm:flex sm:flex-col sm:items-center">
             <div className="border-b border-red-pink mb-3">
-              <h2 className="font-heading text-[35px] mt-3 pb-1">
+              <h2 className="font-heading text-[35px] mt-3 pb-1 sm:text-[40px] sm:w-[560px]">
                 Tiskové zprávy
               </h2>
             </div>
-            <div>
+            <div className="sm:w-[500px]">
               <PressRelease index={1} />
               <PressRelease index={2} />
               <PressRelease index={3} />
@@ -68,14 +68,14 @@ const ProMedia = () => {
           </div>
         </div>
 
-        <div className="w-[350px] mx-auto mb-10">
-          <div className="border-b border-red-pink mb-10">
-            <h2 className="font-heading text-[35px] mt-3 pb-3">
+        <div className="w-[350px] mx-auto mb-10 relative">
+          <div className="border-b border-red-pink mb-10 relative">
+            <h2 className="font-heading text-[35px] mt-3 pb-3 sm:text-[40px]">
               Galerie
             </h2>
           </div>
-          <div className="grid gap-5 relative">
-            <div className="flex absolute z-3 right-10 -top-28">
+          <div className="grid gap-5">
+            <div className="flex absolute z-3 right-3 top-2">
               <Sticker
                 backgroundColor="bg-[#005F66]"
                 textColor="text-light-pink"

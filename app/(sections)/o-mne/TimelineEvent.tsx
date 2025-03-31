ad7ce1a43e2635e9a94629d9740b0fdeeaf8482b
Žofie Tůmová <zofietumova@gmail.com>
2025-03-31 16:05:18 +0200

    sm: timeline dimensions

--- app/(sections)/o-mne/TimelineEvent.tsx
+++ app/(sections)/o-mne/TimelineEvent.tsx
@@ -1,10 +1,12 @@
-const TimelineEvent = ({ eventName, eventDescription }: { eventName: string, eventDescription: string }) => {
+import classNames from "classnames";
+
+const TimelineEvent = ({ eventName, eventDescription, styles = "" }: { eventName: string, eventDescription: string, styles?: string }) => {
   return (
-    <div className="flex flex-col gap-2">
-      <div className="font-heading text-[23px]/[25px]">
+    <div className={classNames(`flex flex-col gap-2 ${styles}`)}>
+      <div className="font-heading text-[23px]/[25px] sm:text-[28px]/[30px] sm:w-70">
         {eventName}
       </div>
-      <div className="text-[14px]/[23px]">
+      <div className="text-[14px]/[23px] sm:text-[16px]/[24px] sm:w-95">
         {eventDescription}
       </div>
     </div>
--- app/(sections)/o-mne/page.tsx
+++ app/(sections)/o-mne/page.tsx
@@ -12,11 +12,36 @@ const OMne = () => {
       <Header />
       <main>
         <div>
-          <div className="w-[350px] mx-auto mt-5 mb-8">
-            <div className="font-heading text-red-pink-light text-[30px]/[36px] mb-3">
-              Jsem Markéta Gregorová.< br />Členkou České pirátské strany< br /> jsem od roku 2012,
+          <div className="w-[350px] sm:w-[560px] md:w-[700px] lg:w-[900px] xl:w-[1100px] 2xl:w-[1400px] 3xl:w-[1660px] mx-auto mt-5 mb-8 flex flex-col sm:grid sm:grid-cols-[3fr_2fr]">
+            <div>
+
+              <div className="font-heading text-red-pink-light text-[30px]/[36px] mb-3 sm:text-[35px]/[41px] ">
+                Jsem Markéta Gregorová.< br />Členkou České pirátské strany< br /> jsem od roku 2012,
+              </div>
+              <div className="w-60 flex ml-25 mb-6 sm:hidden">
+                <Image
+                  src="/o-mne-marketa.png"
+                  height={681}
+                  width={681}
+                  unoptimized
+                  alt="image of Marketa standing by the lectern"
+                />
+              </div>
+
+              <div className="text-white flex flex-col gap-4 text-[14px]/[24px] sm:w-70 sm:text-[16px]/[26px]">
+                <div>
+                  v letech 2018 až 2020 jsem působila jako předsedkyně Evropské pirátské strany. Od roku 2019 pracuji jako poslankyně Evropského parlamentu. V roce 2024 jsem svůj mandát obhájila.
+                </div>
+                <div>
+                  Vystudovala jsem bakalářské studium Mezinárodních vztahů a Evropských studií a magisterské studium Veřejné správy a Bezpečnostních studií. Pracovala jsem jako testerka webových aplikací či markeťačka ve středních i menších firmách, stejně jako v neziskovém sektoru a na vlastní triko.
+                </div>
+                <div>
+                  Zpívám v kapele Definitivní ententýk, miluji svou kočku Kočku a největší radost mi dělá, když lidé volí informovaně.
+                </div>
+              </div>
             </div>
-            <div className="w-60 flex ml-25 mb-6">
+
+            <div className="hidden w-64 flex sm:block pt-10">
               <Image
                 src="/o-mne-marketa.png"
                 height={681}
@@ -25,34 +50,22 @@ const OMne = () => {
                 alt="image of Marketa standing by the lectern"
               />
             </div>
-
-            <div className="text-white flex flex-col gap-4 text-[14px]/[24px]">
-              <div>
-                v letech 2018 až 2020 jsem působila jako předsedkyně Evropské pirátské strany. Od roku 2019 pracuji jako poslankyně Evropského parlamentu. V roce 2024 jsem svůj mandát obhájila.
-              </div>
-              <div>
-                Vystudovala jsem bakalářské studium Mezinárodních vztahů a Evropských studií a magisterské studium Veřejné správy a Bezpečnostních studií. Pracovala jsem jako testerka webových aplikací či markeťačka ve středních i menších firmách, stejně jako v neziskovém sektoru a na vlastní triko.
-              </div>
-              <div>
-                Zpívám v kapele Definitivní ententýk, miluji svou kočku Kočku a největší radost mi dělá, když lidé volí informovaně.
-              </div>
-            </div>
           </div>
         </div>
 
         <div className="bg-beige text-medium-pink pt-6 pb-6">
 
-          <div className="grid grid-cols-[2fr_3fr] w-[350px]">
+          <div className="grid grid-cols-[2fr_3fr] w-[350px] sm:w-[560px] sm:mx-auto sm:grid-cols-[1fr_3fr] sm:gap-3">
 
             <div className="pt-10 pb-12 grid grid-cols-2 relative">
-              <div className="flex flex-col h-240 sm:justify-between pb-12 absolute top-8 left-6">
+              <div className="flex flex-col h-240 sm:justify-between pb-12 absolute top-8 left-6 sm:top-12 sm:left-6 sm:pb-40">
                 <TimelineBulletPoint shape="ellipse" />
-                <TimelineBulletPoint shape="circle" styles="mt-15" />
-                <TimelineBulletPoint shape="ellipse" styles="mt-61" />
-                <TimelineBulletPoint shape="circle" styles="mt-15" />
+                <TimelineBulletPoint shape="circle" styles="mt-15 sm:mt-0" />
+                <TimelineBulletPoint shape="ellipse" styles="mt-61 sm:mt-0" />
+                <TimelineBulletPoint shape="circle" styles="mt-15 sm:mt-0" />
               </div>
-              <div className="w-1 h-240 bg-red-pink absolute left-28 relative">
-                <div className="w-6 absolute -top-5 -left-2.5">
+              <div className="w-1 h-240 bg-red-pink absolute left-28 relative sm:left-32 sm:h-260">
+                <div className="w-6 absolute -top-5 -left-2.5 sm:w-8 sm:-left-3.5">
                   <Image
                     src="/cat-timeline-icon.svg"
                     height={54}
@@ -60,7 +73,7 @@ const OMne = () => {
                     alt="start of the timeline icon shaped like a cat"
                   />
                 </div>
-                <div className="w-6 absolute -bottom-5 -left-2.5">
+                <div className="w-6 absolute -bottom-5 -left-2.5 sm:w-8 sm:-left-3.5">
                   <Image
                     src="/cat-timeline-icon.svg"
                     height={54}
@@ -73,11 +86,27 @@ const OMne = () => {
             </div>
 
             <div>
-              <div className="flex flex-col h-240 justify-around pt-8">
-                <TimelineEvent eventName="Vstup do České pirátské strany" eventDescription="Markéta Gregorová se stala členkou České pirátské strany v roce 2012." />
-                <TimelineEvent eventName="Lorem ipsum stup do České pirátské strany" eventDescription="Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012." />
-                <TimelineEvent eventName="Vstup do České pirátské strany" eventDescription="Markéta Gregorová se stala členkou České pirátské strany v roce 2012." />
-                <TimelineEvent eventName="Lorem ipsum stup do České pirátské strany" eventDescription="Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012." />
+              <div className="flex flex-col h-260 justify-around pt-8 sm:pt-12">
+                <TimelineEvent
+                  eventName="Vstup do České pirátské strany"
+                  eventDescription="Markéta Gregorová se stala členkou České pirátské strany v roce 2012."
+                  styles="mb-31"
+                />
+                <TimelineEvent
+                  eventName="Lorem ipsum stup do České pirátské strany"
+                  eventDescription="Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012."
+
+                />
+                <TimelineEvent
+                  eventName="Vstup do České pirátské strany"
+                  eventDescription="Markéta Gregorová se stala členkou České pirátské strany v roce 2012."
+                  styles="sm:mb-32"
+                />
+                <TimelineEvent
+                  eventName="Lorem ipsum stup do České pirátské strany"
+                  eventDescription="Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012.Markéta Gregorová se stala členkou České pirátské strany v roce 2012."
+                  styles="sm:mb-8"
+                />
               </div>
 
             </div>

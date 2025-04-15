3ac0d538d926d9493c64b14c8c5769f9502cd1fa
Žofie Tůmová <zofietumova@gmail.com>
2025-04-15 16:11:38 +0200

    share buttons init

--- app/(sections)/clanky/[slug]/page.tsx
+++ app/(sections)/clanky/[slug]/page.tsx
@@ -14,6 +14,20 @@ export async function generateMetadata(
   return {
     title: article?.title || "Markéta Gregorová",
     description: article?.perex,
+    openGraph: {
+      title: article.title,
+      description: article.perex,
+      url: `https://www.gregorova.eu${article.link}`,
+      type: "article",
+      images: [
+        {
+          url: `https://www.gregorova.eu${article.img}`,
+          width: 1200,
+          height: 630,
+          alt: article.title,
+        },
+      ],
+    },
   };
 }
 
@@ -24,14 +38,16 @@ const Article = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const article = data.find((obj: ArticleType) => obj.link.includes(slug));
   const markup = { __html: article.content };
   const baseUrl = "www.gregorova.eu";
-  const urlForXSharing = `${baseUrl}${article.link}`;
-  console.log(urlForXSharing);
+  const urlForSharing = `${baseUrl}${article.link}`;
 
   return (
     <div className="bg-beige">
       <HeaderMobile />
       <Header />
       <main className="text-medium-pink mt-4">
+        <script async src="https://platform.twitter.com/widgets.js" />
+        <div id="fb-root" />
+        <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v22.0" />
         <div className="flex flex-col main-width mx-auto mb-10">
           <div className="flex flex-col gap-2 border-b border-light-pink pb-4 mb-4">
             <Link
@@ -45,14 +61,45 @@ const Article = async ({ params }: { params: Promise<{ slug: string }> }) => {
           <h1 className="font-heading text-[35px]/[41px] mb-3">{article.title}</h1>
           <div className="font-bold mb-4">{article.perex}</div>
           <article dangerouslySetInnerHTML={markup} className="article" />
-          <Link
-            href="/clanky"
-            className="bg-medium-pink rounded-full text-[14px] text-white font-semibold italic text-center w-fit h-fit px-4 py-1 mr-6 3xl:px-7 3xl:py-2.5"
-          >
-            zpět na články
-          </Link>
-          <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-show-count="false">Tweet</a>
-          <script async src="https://platform.twitter.com/widgets.js" />
+          <div className="flex justify-between">
+            <Link
+              href="/clanky"
+              className="bg-medium-pink rounded-full text-[14px] text-white font-semibold italic text-center w-fit h-fit px-4 py-1 mr-6 3xl:px-7 3xl:py-2.5"
+            >
+              zpět na články
+            </Link>
+            <div className="flex items-center gap-4">
+              <div>
+                <a
+                  href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+                  className="twitter-share-button"
+                  data-show-count="false"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Tweet
+                </a>
+              </div>
+
+              <div
+                className="fb-share-button"
+                data-href="https://www.gregorova.eu"
+                data-layout=""
+                data-size=""
+              >
+                <a
+                  target="_blank"
+                  href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlForSharing)}`}
+                  className="fb-xfbml-parse-ignore"
+                  rel="noreferrer"
+                >
+                  Sdílet
+                </a>
+              </div>
+
+            </div>
+          </div>
+
         </div>
       </main>
 

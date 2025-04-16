0d76e0c4d49d30350cafd6fe0e490b8257efe327
Žofie Tůmová <zofietumova@gmail.com>
2025-04-16 14:46:32 +0200

    article detail -lg, md fix

--- app/(sections)/clanky/[slug]/page.tsx
+++ app/(sections)/clanky/[slug]/page.tsx
@@ -49,28 +49,28 @@ const Article = async ({ params }: { params: Promise<{ slug: string }> }) => {
         <div id="fb-root" />
         <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v22.0" />
         <div className="flex flex-col main-width mx-auto mb-10 sm:mb-12 md:mb-16">
-          <div className="flex flex-col gap-1 border-b border-light-pink pb-4 mb-3 sm:pb-5">
+          <div className="flex flex-col gap-1 border-b border-light-pink pb-4 mb-3 sm:pb-5 lg:mb-5 lg:mt-5 lg:gap-2">
             <Link
               href="/clanky"
-              className="bg-medium-pink rounded-full text-[14px] text-white font-semibold italic text-center w-fit h-fit px-4 py-1 mr-6 md:text-base 3xl:px-7 3xl:py-2.5"
+              className="bg-medium-pink rounded-full text-sm text-white font-semibold italic text-center w-fit h-fit px-4 py-1 mr-6 md:text-base lg:text-lg 3xl:px-7 3xl:py-2.5"
             >
               zpět na články
             </Link>
-            <div className="ml-4 italic text-sm md:text-base">
+            <div className="ml-4 italic text-sm md:text-base lg:text-lg">
               {article.date}
             </div>
           </div>
-          <h1 className="font-heading text-4xl mb-3 sm:text-5xl sm:w-130 sm:mx-auto">
+          <h1 className="font-heading text-4xl mb-3 sm:text-5xl sm:w-130 sm:mx-auto md:text-6xl md:w-162 md:mb-4 lg:text-7xl lg:w-196">
             {article.title}
           </h1>
-          <div className="font-bold mb-2 text-lg sm:w-94 sm:mx-auto sm:mb-1 md:w-110">
+          <div className="font-bold mb-2 text-lg sm:w-94 sm:mx-auto sm:mb-1 md:w-110 md:text-xl md:w-120 md:mb-2 lg:text-2xl lg:w-144">
             {article.perex}
           </div>
           <article dangerouslySetInnerHTML={markup} className="article" />
-          <div className="flex justify-between sm:w-100 sm:mx-auto sm:mt-2 md:w-114 md:mt-4">
+          <div className="flex justify-between items-center sm:w-100 sm:mx-auto sm:mt-2 md:w-120 md:mt-4 lg:w-144 ">
             <Link
               href="/clanky"
-              className="bg-medium-pink rounded-full text-base text-white font-semibold italic text-center w-fit h-fit px-4 py-1 mr-6 md:text-lg 3xl:px-7 3xl:py-2.5"
+              className="bg-medium-pink rounded-full text-base text-white font-semibold italic text-center w-fit h-fit px-4 py-1 mr-6 md:text-base lg:text-lg 3xl:px-7 3xl:py-2.5"
             >
               zpět na články
             </Link>
@@ -88,7 +88,7 @@ const Article = async ({ params }: { params: Promise<{ slug: string }> }) => {
               </div>
 
               <div
-                className="fb-share-button"
+                className="fb-share-button self-center"
                 data-href="https://www.gregorova.eu"
                 data-layout=""
                 data-size=""

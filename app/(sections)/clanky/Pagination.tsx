34f22087cc492c61d48947edb88f841504daabcb
Žofie Tůmová <zofietumova@gmail.com>
2025-04-23 15:33:16 +0200

    pagination styles, arrows added

--- app/(sections)/clanky/Pagination.tsx
+++ app/(sections)/clanky/Pagination.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useSearchParams, useRouter } from "next/navigation";
 
 type PaginationProps = {
@@ -27,21 +28,60 @@ const Pagination = ({ numberOfArticles, perPage = 10 }: PaginationProps) => {
   const arrayOfPages = Array.from({ length: numberOfPages }, (_, i) => i + 1);
   console.log(arrayOfPages);
 
+  const maxVisible = 7;
+  let start = Math.max(1, currentPage - Math.floor(maxVisible / 2));
+  let end = start + maxVisible - 1;
+
+  if (end > numberOfPages) {
+    end = numberOfPages;
+    start = Math.max(1, end - maxVisible + 1);
+  }
+
+  const visiblePages = arrayOfPages.slice(start - 1, end);
+
   return (
-    <div className="mb-10 flex justify-center gap-3">
-      {arrayOfPages.map((num) => (
+    <div className="mb-10 flex justify-center gap-2">
+      {currentPage > 1 && (
+        <button
+          type="button"
+          onClick={() => handlePageClick(currentPage - 1)}
+          className="px-3 text-2xl text-medium-pink hover:text-white hover:bg-medium-pink rounded"
+        >
+          <Image
+            src="/arrow-pagination.svg"
+            height={25}
+            width={25}
+            alt="arrow back"
+          />
+        </button>
+      )}
+      {visiblePages.map((num) => (
         <button
           type="button"
           key={num}
           onClick={() => handlePageClick(num)}
-          className={`px-3 py-1 rounded ${num === currentPage
+          className={`font-heading text-3xl px-2 ${num === currentPage
             ? "bg-medium-pink text-white"
-            : "bg-light-pink text-medium-pink"
-            } hover:bg-pink-200 transition`}
+            : "text-medium-pink"} hover:bg-pink-200 transition cursor-pointer`}
         >
           {num}
         </button>
       ))}
+      {currentPage < numberOfPages && (
+        <button
+          type="button"
+          onClick={() => handlePageClick(currentPage + 1)}
+          className="px-3 text-2xl text-medium-pink hover:text-white hover:bg-medium-pink rounded"
+        >
+          <Image
+            src="/arrow-pagination.svg"
+            height={25}
+            width={25}
+            alt="arrow forward"
+            className="transition-transform rotate-180"
+          />
+        </button>
+      )}
     </div>
   );
 };

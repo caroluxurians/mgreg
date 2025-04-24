535a9c7d9abe8b951389297c3d87a081fdfab903
Žofie Tůmová <zofietumova@gmail.com>
2025-04-24 14:40:18 +0200

    socials - themes, links

--- app/components/SocialsSet.tsx
+++ app/components/SocialsSet.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import FacebookIcon from "./svg/FacebookIcon";
 import InstagramIcon from "./svg/InstagramIcon";
 import TwitterIcon from "./svg/TwitterIcon";
@@ -7,35 +8,48 @@ const SocialsSet = ({ color = "dark", classNames = "" }: { color?: string, class
   const classNameDark = "text-medium-pink hover:text-header-hover-pink transition-colors duration-100";
   const classNameLight = "text-white hover:text-header-hover-pink transition-colors duration-100";
 
+  const instagramLink = "https://www.instagram.com/ruzovarebelka/";
+  const twitterLink = "https://x.com/marketkag";
+  const youtubeLink = "https://www.youtube.com/channel/UCcf12G_5igxOSZBFSt4eEAQ";
+  const facebookLink = "https://www.facebook.com/gregorova.marketka";
+
   return (
     <div className={`${classNames} gap-[30px]`}>
       {color === "dark"
         ? (
           <>
-            <InstagramIcon className={classNameDark} />
-            <TwitterIcon className={classNameDark} />
-            <YoutubeIcon className={classNameDark} />
-            <FacebookIcon className={classNameDark} />
-
+            <Link href={instagramLink} target="_blank">
+              <InstagramIcon className={classNameDark} />
+            </Link>
+            <Link href={twitterLink} target="_blank">
+              <TwitterIcon className={classNameDark} />
+            </Link>
+            <Link href={youtubeLink} target="_blank">
+              <YoutubeIcon className={classNameDark} />
+            </Link>
+            <Link href={facebookLink} target="_blank">
+              <FacebookIcon className={classNameDark} />
+            </Link>
           </>
         )
         : (
           <>
-            <InstagramIcon className={classNameLight} />
-            <TwitterIcon className={classNameLight} />
-            <YoutubeIcon className={classNameLight} />
-            <FacebookIcon className={classNameLight} />
+            <Link href={instagramLink} target="_blank">
+              <InstagramIcon className={classNameLight} />
+            </Link>
+            <Link href={twitterLink} target="_blank">
+              <TwitterIcon className={classNameLight} />
+            </Link>
+            <Link href={youtubeLink} target="_blank">
+              <YoutubeIcon className={classNameLight} />
+            </Link>
+            <Link href={facebookLink} target="_blank">
+              <FacebookIcon className={classNameLight} />
+            </Link>
           </>
         )}
-
     </div>
   );
 };
 
 export default SocialsSet;
-
-/*  <Image src="/facebook-dark.svg" height={21} width={9} alt="facebook icon" />
-<Image src="/instagram-dark.svg" height={21} width={21} alt="instagram icon" className="" />
- <Image src="/x-dark.svg" height={21.34} width={23} alt="x icon" />
- <Image src="/youtube-dark.svg" height={21.38} width={30.54} alt="youtube icon" />
- */

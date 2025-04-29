002cd0cf2c33078511990feaf8070fc84192339d
Žofie Tůmová <zofietumova@gmail.com>
2025-04-29 14:22:41 +0200

    starsi - buttons inserted, layout styles

--- app/(sections)/pro-media/starsi/page.tsx
+++ app/(sections)/pro-media/starsi/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Header from "@/app/components/Header";
 import HeaderMobile from "@/app/components/HeaderMobile";
+import BackToGalleryButton from "../BackToGalleryButton";
+import DownloadPhotosButton from "../DownloadPhotosButton";
 
 const images = [
   "2022_12_15_marketa_Gregorova_1920px-4549.jpg",
@@ -30,9 +32,15 @@ const Starsi = () => {
       <HeaderMobile />
       <Header />
       <main className="flex flex-col">
-        <div className="main-width mx-auto mb-12">
-          <h2 className="font-heading title-font-size pb-2  lg:pb-1 xl:pb-2 3xl:pb-3">Starší</h2>
-          <div className="flex flex-col items-center gap-2">
+        <div className="main-width mx-auto mt-5 mb-14">
+          <div className="flex justify-between mb-4">
+            <BackToGalleryButton />
+            <DownloadPhotosButton />
+          </div>
+          <h2 className="font-heading title-font-size border-b border-red-pink pb-2 mb-4 lg:pb-1 xl:pb-2 3xl:pb-3">
+            Starší
+          </h2>
+          <div className="flex flex-col items-center gap-2 mb-8">
             {images.map((image) => (
               <a
                 href={`/gallery/starsi/${image}`}
@@ -46,12 +54,15 @@ const Starsi = () => {
                     height={200}
                     width={400}
                     alt="older photo"
-                    className="rounded-lg"
+                    className="rounded-lg transition-transform hover:scale-105 duration-300"
                   />
                 </div>
               </a>
             ))}
-
+          </div>
+          <div className="flex justify-between">
+            <BackToGalleryButton />
+            <DownloadPhotosButton />
           </div>
         </div>
       </main>

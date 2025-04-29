5cf4c185350f6042c8ed3e13741de300d786c11b
Žofie Tůmová <zofietumova@gmail.com>
2025-04-29 16:36:42 +0200

    download button -breakpoint widths

--- app/(sections)/pro-media/DownloadPhotosButton.tsx
+++ app/(sections)/pro-media/DownloadPhotosButton.tsx
@@ -5,7 +5,7 @@ const DownloadPhotosButton = ({ fileSize = "40 MB" }: { fileSize?: string }) => 
   return (
     <Link
       href="/pro-media#gallery"
-      className="flex items-center justify-center text-white bg-medium-pink hover:text-medium-pink hover:bg-white transition-colors duration-100 rounded-full text-sm font-semibold italic text-center h-8 w-24 md:w-38 md:h-9 lg:w-44 lg:h-10.5 2xl:w-48 2xl:h-12 md:text-base lg:text-lg 2xl:text-xl"
+      className="flex items-center justify-center text-white bg-medium-pink hover:text-medium-pink hover:bg-white transition-colors duration-100 rounded-full text-sm font-semibold italic text-center h-8 w-24 md:w-28 md:h-9 lg:w-32 lg:h-10.5 2xl:w-36 2xl:h-12 md:text-base lg:text-lg 2xl:text-xl"
     >
       <Download className="w-5 h-5" />
       {`${fileSize}`}

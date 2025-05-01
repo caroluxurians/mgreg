ae57e8d6d6931b3bb4b432c42bc5fc7f2c96562b
Žofie Tůmová <zofietumova@gmail.com>
2025-05-01 16:46:57 +0200

    tomas contact card - flip, styles xs-md

--- app/(sections)/pro-media/ContactCardTomas.tsx
+++ app/(sections)/pro-media/ContactCardTomas.tsx
@@ -1,28 +1,97 @@
+"use client";
+
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const ContactCardTomas = () => {
-  return (
-    <div className="bg-medium-pink overflow-hidden rounded-3xl flex w-[270px] h-[359px] relative mx-auto md:w-[200px] md:h-[266px] lg:w-[250px] lg:h-[332px] xl:w-[300px] xl:h-[399px] 2xl:w-[325px] 2xl:h-[432px]">
-      <div className="absolute -bottom-18 w-82 -right-9 md:w-62 md:-bottom-14 md:-right-8 lg:w-74 xl:w-82 xl:-bottom-14 2xl:w-94 2xl:-right-10 2xl:-bottom-18">
-        <Image
-          src="/tomas-contact-card-new.png"
-          height={432}
-          width={325}
-          alt="Tomas Polak contact card"
-          unoptimized
-          className="rounded-[30px] w-full h-auto md:rounded-[20px]"
-        />
-      </div>
+  const [flipped, setFlipped] = useState(false);
+  const pathname = usePathname();
 
-      <div className="absolute top-5 left-6 flex flex-col md:top-4 md:left-4 xl:top-5 xl:left-6 2xl:top-6 2xl:left-7">
-        <div className="font-heading text-beige text-[40px]/[40px] md:text-[30px]/[30px] lg:text-[38px]/[38px] xl:text-[46px]/[46px] 2xl:text-[50px]/[50px]">
-          Tomáš Polák
+  useEffect(() => {
+    setFlipped(false);
+  }, [pathname]);
+
+  return (
+    <button
+      type="button"
+      className="relative w-68 h-90 md:w-50 md:h-67 lg:w-62 lg:h-83 xl:w-75 xl:h-100 2xl:w-81 2xl:h-108 [perspective:1000px] mx-auto bg-transparent"
+      onClick={() => setFlipped(!flipped)}
+      aria-pressed={flipped}
+      tabIndex={0}
+    >
+      <div
+        className={`
+          relative w-full h-full transition-transform duration-700 [transform-style:preserve-3d]
+          ${flipped ? "[transform:rotateY(180deg)]" : ""}
+        `}
+      >
+        {/* Front Face */}
+        <div className="absolute bg-medium-pink w-full h-full overflow-hidden rounded-3xl flex [backface-visibility:hidden]">
+          <div className="absolute -bottom-18 w-82 -right-9 md:w-62 md:-bottom-14 md:-right-8 lg:w-74 xl:w-82 xl:-bottom-14 2xl:w-94 2xl:-right-10 2xl:-bottom-18">
+            <Image
+              src="/tomas-contact-card-new.png"
+              height={432}
+              width={325}
+              alt="Tomas Polak contact card"
+              unoptimized
+              className="w-full h-auto"
+            />
+          </div>
+
+          <div className="absolute top-5 left-6 flex flex-col md:top-4 md:left-4 xl:top-5 xl:left-6 2xl:top-6 2xl:left-7">
+            <div className="font-heading text-beige text-4xl md:text-3xl lg:text-4xl xl:text-5xl">
+              Tomáš Polák
+            </div>
+            <div className="text-light-pink text-left text-lg md:text-sm lg:text-base xl:text-lg 2xl:text-xl">
+              PR specialist
+            </div>
+          </div>
         </div>
-        <div className="text-light-pink text-[18px]/[22px] md:text-[14px]/[16px] lg:text-[16px]/[18px] xl:text-[18px]/[24px] 2xl:text-[20px]/[30px]">
-          PR specialist
+        {/* Back Face */}
+        <div className="absolute inset-0 w-full h-full bg-medium-pink rounded-3xl flex flex-col items-center justify-center [backface-visibility:hidden] [transform:rotateY(180deg)]">
+          <div className="absolute top-5 left-6 flex flex-col md:top-4 md:left-4 xl:top-5 xl:left-6 2xl:top-6 2xl:left-7">
+            <div className="font-heading text-beige text-4xl md:text-3xl lg:text-4xl xl:text-5xl">
+              Tomáš Polák
+            </div>
+            <div className="text-light-pink text-left font-medium text-lg md:text-sm lg:text-base xl:text-lg 2xl:text-xl">
+              PR specialist
+            </div>
+          </div>
+          <div className="flex flex-col absolute top-36 left-6 md:top-25 md:left-4 lg:top-32 xl:top-40 xl:left-6 2xl:left-7 2xl:top-42">
+            <div
+              className="text-white text-left mb-10 w-60
+                md:text-sm md:mb-2 md:w-42
+                lg:text-base lg:w-auto lg:mb-14
+                xl:text-lg xl:mb-16 2xl:text-xl 2xl:mb-20"
+            >
+              Potřebujete mě kontaktovat?
+              Rád odpovím na vaše
+              dotazy nebo připomínky.
+            </div>
+            <div className="flex flex-col gap-3 md:gap-2">
+              <a
+                href="mailto:tomas.polak@europarl.europa.eu"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className="text-white font-black text-base flex flex-col text-left hover:underline md:text-sm lg:text-lg 2xl:text-xl">
+                  <span>tomas.polak</span>
+                  <span>@europarl.europa.eu</span>
+                </div>
+              </a>
+              <a
+                href="tel:+420728035059"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className="text-white font-black text-base text-left hover:underline md:text-sm lg:text-lg 2xl:text-xl">
+                  +420 728 035 059
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
 
